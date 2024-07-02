a46809d886fdc7dafcc5e952e59adb31a13d2e28
Fulgur <fulgurgithub@gmail.com>
2024-07-02 08:40:11 +0200

    Added default pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/privacy', function () {
+    return view('privacy');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
