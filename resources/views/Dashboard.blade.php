7b5e51b35757045e54735b7571e8e9fe469174cc
Fulgur <fulgurgithub@gmail.com>
2024-07-02 15:20:28 +0200

    Closing p tag

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
- <p>Welcome to {{ $_ENV['APP_NAME']}}, {{ auth()->user()->name }}!
+ <p>Welcome to {{ $_ENV['APP_NAME']}}, {{ auth()->user()->name }}!</p>
 @endsection
