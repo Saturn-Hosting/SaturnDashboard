6dc8e32c40a35caa2d653614dd7f2d0f098ab23b
Fulgur <fulgurgithub@gmail.com>
2024-07-02 08:34:26 +0200

    Nav

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$_ENV['APP_NAME']}}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/privacy">Privacy</a>
+                <a href="/contact">Contact</a>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
