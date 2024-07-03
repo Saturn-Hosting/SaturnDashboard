2c609739654f241deb2831c17c3554ac2f0baaec
Fulgur <fulgurgithub@gmail.com>
2024-07-03 15:14:32 +0200

    Basic dashboard with example projects

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
- <p>Welcome to {{ $_ENV['APP_NAME']}}, {{ auth()->user()->name }}!</p>
+    <div class="dashboard">
+        <h2>Dashboard</h2>
+        <p>Welcome to {{ $_ENV['APP_NAME'] }}, {{ auth()->user()->name }}!</p>
+        <a href="#" class="button">Create new project</a>
+        <h3>Projects</h3>
+        <div class="cards-grid">
+            <?php for ($i = 0; $i < 10; $i++) { ?>
+                <!-- example project placeholder -->
+                <div class="card">
+                    <h3>Example project</h3>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 @endsection
