6502025862d2b475a8f364a6d004dc404d5f3ff9
Fulgur <fulgurgithub@gmail.com>
2024-07-03 14:58:50 +0200

    Header styling fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,21 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$_ENV['APP_NAME']}} - Dashboard</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- scripts -->
+    <script src="https://kit.fontawesome.com/837e657fcc.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
         <nav>
+            <h1>{{$_ENV['APP_NAME']}}</h1>
             <ul>
-                <a href="/app">Home</a>
-                <a href="/logout">Logout</a>
+                <div class="seperate"></div>
+                <div class="seperate">
+                    <a href="/app"><i class="fa-solid fa-house"></i></a>
+                    <a href="/logout"><i class="fa-solid fa-right-from-bracket"></i></a>
+                </div>
             </ul>
         </nav>
     </header>
     <main>
         @yield('content')
     </main>
-    <footer>
-        <p class="trademark">&copy; {{date('Y')}} {{$_ENV['APP_NAME']}}</p>
-    </footer>
 </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$_ENV['APP_NAME']}}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/guest.css', 'resources/js/guest.js'])
 </head>
 <body>
     <header>
