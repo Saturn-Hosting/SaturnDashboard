78e6759f5d15d084a4d07ae8a9bfdc012a17bcdf
AzEdownload <contact@aze.email>
2024-07-14 16:08:49 +0200

    Fixed card sizing

--- resources/views/livewire/show-projects.blade.php
+++ resources/views/livewire/show-projects.blade.php
@@ -1,6 +1,6 @@
 <div>
     @foreach ($projects as $project)
-        <div class="card p-5 border-neutral-700 border rounded-lg justify-center align-middle text-center w-37">
+        <div class="card p-5 border-neutral-700 border rounded-lg justify-center align-middle text-center w-36">
             <h3>{{ $project->name }}</h3>
         </div>
     @endforeach
