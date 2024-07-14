58ec6f323fc5c17f93cea3f4574646ba5d4c9492
Lennart <fulgurgithub@gmail.com>
2024-07-14 16:31:46 +0200

    Bugfix: projects not showing in a grid

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -7,11 +7,8 @@
         <br>
         <h3 class="text-2xl">Projects</h3>
         <br>
-        <div class="grid grid-cols-4 gap-4 cards-grid">
+        <div>
             <livewire:show-projects />
-            <a href="{{ route('projects.create') }}" class="card p-5 border rounded-lg w-36 justify-center align-middle text-center">
-                <h3 class="text-2xl">+</h3>
-            </a>
         </div>
     </div>
 @endsection
--- resources/views/livewire/show-projects.blade.php
+++ resources/views/livewire/show-projects.blade.php
@@ -1,7 +1,10 @@
-<div>
+<div class="grid grid-cols-4 gap-4 cards-grid">
     @foreach ($projects as $project)
         <div class="card p-5 border-neutral-700 border rounded-lg justify-center align-middle text-center w-36">
             <h3>{{ $project->name }}</h3>
         </div>
     @endforeach
+    <a href="{{ route('projects.create') }}" class="card p-5 border rounded-lg w-36 justify-center align-middle text-center">
+        <h3 class="text-2xl">+</h3>
+    </a>
 </div>
