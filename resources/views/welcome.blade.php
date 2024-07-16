0738b93c784db32d03fa45016635b30dba167bf2
AzEdownload <contact@aze.email>
2024-07-16 14:33:20 +0200

    Simple responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,10 @@
         <div class="twinkling"></div>
             <div class="flex items-center justify-between w-full">
                 <!-- Left side content -->
-                <div class="flex flex-col items-center justify-center w-1/3 ml-32">
-                    <h2 class="text-7xl font-extrabold mb-5 overflow-hidden" id="home-title">Saturn Hosting</h2>
-                    <p class="text-gray-300 text-xl overflow-hidden">Discover the power of cloud computing with Saturn Hosting.</p>
-                    <p class="text-gray-300 text-xl mb-6 overflow-hidden">Our platform offers scalable solutions to your needs.</p>
+                <div class="flex flex-col items-center justify-center w-4/12 ml-32">
+                    <h2 class="2xl:text-6xl lg:text-5xl font-extrabold mb-5 overflow-hidden" id="home-title">Saturn Hosting</h2>
+                    <p class="text-gray-300 2xl:text-xl lg:text-lg overflow-hidden">Discover the power of cloud computing with Saturn Hosting.</p>
+                    <p class="text-gray-300 2xl:text-xl lg:text-lg mb-6 overflow-hidden">Our platform offers scalable solutions to your needs.</p>
                     <a href="/app">
                         <button onclick="" class="text-white bg-black border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                             <p class="font-bold text-lg">Get Started ></p>
@@ -19,8 +19,8 @@
                     </a>
                 </div>
                 <!-- Right side content -->
-                <div class="flex flex-col items-center justify-center w-2/3 ml-auto">
-                    <img src="storage/img/saturn_big.png" alt="" class="w-full/2">
+                <div class="flex flex-col items-center justify-center w-8/12 ml-auto">
+                    <img src="storage/img/saturn_big.png" alt="" class="w-3/4">
                 </div>
         </div>
     </section>
