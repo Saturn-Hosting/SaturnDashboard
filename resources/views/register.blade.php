b00ed8be02a74e711a49e10788fefb20967511fd
AzEdownload <contact@aze.email>
2024-07-30 16:21:09 +0200

    Fixed the design of login/register pages to fit the theme better.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <div class="user-form">
+    <div class="stars"></div>
+    <div class="twinkling"></div>
     <section class="bg-gray-50 dark:bg-black" >
         <div class="flex flex-col items-center justify-center px-32 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-xl xl:p-0 dark:bg-neutral-950 dark:border-gray-700">
@@ -22,9 +24,9 @@
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-neutral-950 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                         </div>
-                        <button type="submit" class="w-full text-white bg-blue-500 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Register</button>
+                        <button type="submit" class="w-full text-white border-white border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-primary-700 dark:focus:ring-primary-800 hover:bg-neutral-800">Register</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account? <a href="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                            Already have an account? <a href="/login" class="font-medium text-neutral-200 underline">Login here</a>
                         </p>
                     </form>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
                     <h2 class="2xl:text-6xl lg:text-5xl font-extrabold mb-5 overflow-hidden" id="home-title">Saturn Hosting</h2>
                     <p class="text-gray-300 3xl:text-xl lg:text-lg overflow-hidden mb-6">Discover the power of cloud computing.</p>
                     <a href="/app">
-                        <button onclick="" class="text-white bg-black border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        <button onclick="" class="hover:bg-neutral-800 text-white bg-black border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                             <p class="font-bold text-lg">Get Started ></p>
                         </button>
                     </a>
